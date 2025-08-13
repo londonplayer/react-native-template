bf04e74ed054cd87b9102db8af827f2d1cf69140
londonplayer <jvgerhardt@gmail.com>
2025-08-13 04:13:57 -0300

    feat: nativewind/tailwind/rn-reanimated

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<View className="flex-1 items-center justify-center bg-gray-100">
+			<Text className="text-5xl text-blue-500 p-6">Start editing your home page.</Text>
+		</View>
+	);
 }
