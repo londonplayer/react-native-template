66fb6d36f081ffe6e904c49c981a2922d1626373
londonplayer <jvgerhardt@gmail.com>
2025-08-13 04:50:47 -0300

    feat: custom styles

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
 	return (
 		<View className="flex-1 items-center justify-center bg-gray-100">
-			<Text className="text-5xl text-blue-500 p-6">Start editing your home page.</Text>
+			<Text className="heading-text-h1 text-primary p-6">Start editing your home page.</Text>
 		</View>
 	);
 }
